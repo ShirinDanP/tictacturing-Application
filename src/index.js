eeda39cc455bad95a47224378fc754d80cbbf437
Shirin Danehpash <shdan1@mma-c02tpfyxg8wn.global.ad>
2017-09-18 14:18:03 +0200

    our first heroku commit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 import {Router, browserHistory} from 'react-router';
-import Routes from './routes/index.js';
+import Routes from './routes';
 
 ReactDOM.render(
     <Router
